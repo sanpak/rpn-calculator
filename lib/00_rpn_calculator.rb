fe65ede94a3bf9dc74d158bb947f46d1719305ea
SANPAK Ng <sanpakng@SANPAKs-Air.fios-router.home>
2017-05-29 11:22:19 -0400

    RPN in the morning

--- lib/00_rpn_calculator.rb
+++ lib/00_rpn_calculator.rb
@@ -1,3 +1,38 @@
 class RPNCalculator
   # TODO: your code goes here!
+  attr_accessor :number
+
+  def initialize
+    @stack = []
+  end
+
+  def push(number)
+    @stack << number
+  end
+
+  def plus
+    @stack << @stack.pop + @stack.pop
+  end
+
+  def minus
+    temp = @stack.pop
+    @stack << @stack.pop - temp
+  end
+
+  def divide
+  end
+
+  def value
+    @stack[-1]
+  end
+
+  def times
+    @stack << @stack.pop * @stack.pop
+  end
+
+  def tokens
+  end
+
+  def evaluate
+  end
 end
